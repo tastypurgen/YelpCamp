8f59c7bc6023d95e7e48f0f937baaa7fd4313b58
tastypurgen <stilegs@gmail.com>
2019-11-05 14:12:23 +0300

    comments update

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ mongoose.connect("mongodb://localhost:27017/yelpcamp", {useNewUrlParser: true});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
 
 app.get('/', (req, res) => {
