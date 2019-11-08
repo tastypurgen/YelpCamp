669b5c3a2c6fae125f903eb97b55289ab35861d0
tastypurgen <stilegs@gmail.com>
2019-11-08 13:13:54 +0300

    add comment delete

--- routes/comments.js
+++ routes/comments.js
@@ -34,7 +34,7 @@ router.post('/', isLoggedIn, (req, res) => {
 });
 
 // EDIT
-router.get('/:comment_id/edit', (req, res) => {
+router.get('/:comment_id/edit', checkCommentOwnership, (req, res) => {
 	Comment.findById(req.params.comment_id, (err, foundComment) => {
 		if (err) res.redirect('back');
 		else {
@@ -44,7 +44,7 @@ router.get('/:comment_id/edit', (req, res) => {
 });
 
 
-router.put('/:comment_id', (req, res) => {
+router.put('/:comment_id', checkCommentOwnership, (req, res) => {
 	Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, foundComment) => {
 		if (err) res.redirect('back');
 		else {
@@ -52,6 +52,34 @@ router.put('/:comment_id', (req, res) => {
 		}
 	});
 });
+
+
+// DELETE
+ router.delete('/:comment_id', checkCommentOwnership, (req, res) => {
+	Comment.findByIdAndDelete(req.params.comment_id, (err) => {
+		if (err) res.redirect('/campgrounds');
+		else {
+			res.redirect('back');
+		}
+	});
+ });
+
+function checkCommentOwnership(req, res, next) {
+	if (req.isAuthenticated()) {
+		Comment.findById(req.params.comment_id, (err, foundComment) => {
+			if (err) res.redirect('back');
+			else {
+				if (foundComment.author.id.equals(req.user._id)) {
+					next();
+				} else {
+					res.redirect('back');
+				}
+			}
+		});
+	} else {
+		res.redirect('back');
+	}
+}
 
 function isLoggedIn(req, res, next) {
 	if (req.isAuthenticated()) return next();
